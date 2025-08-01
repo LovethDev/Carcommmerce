b0e16327c26583657246bc3035421f80835b9162
Loveth Obaje <obajeufedo2@gmail.com>
2025-08-01 02:05:34 +0100

    Make navbar fixed and allow body scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { ContactView } from './components/ContactView'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 pt-16">
       <Header />
       
       <Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-gradient-to-r from-gray-900 to-gray-800 shadow-lg border-b border-gray-700 font-inter">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-gray-900 to-gray-800 shadow-lg border-b border-gray-700 font-inter">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo and Brand Name */}
